3d9795b935f4a1ae6efde28051cfd876edbd9b8a
Pontus Palmenäs <pikzel@gmail.com>
2015-02-02 23:55:52 +0100

    Optimize for Python 2.x

--- zipfsong.py
+++ zipfsong.py
@@ -1,10 +1,10 @@
 # Small python hack for Spotify Puzzle 'zipfsong', by Pontus Palmenäs
 n, m = map(int, raw_input().split())
 songs = []
-for i in range(1, n + 1):
+for i in xrange(1, n + 1):
     f, s = raw_input().split()
     q = float(f) * i
     songs.append((i, q, s))
 songs.sort(key=lambda x: x[1], reverse=True)
-for i in range(m):
+for i in xrange(m):
     print(songs[i][2])
